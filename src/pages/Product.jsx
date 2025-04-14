d301741161db83a085629138017d36e413e00186
rayxona <rayxona@gmail.com>
2025-04-14 08:08:23 +0500

    feat: enhance Product page with detailed product display and styling

--- src/pages/Product.jsx
+++ src/pages/Product.jsx
@@ -9,7 +9,19 @@ export const loader = async ({ params }) => {
 function Product() {
   const data = useLoaderData();
   console.log(data);
-  return <div></div>;
+  return (
+    <div className="container py-26 px-8">
+      <div className="product-card">
+        <img className="product-thubmnail" src={data.thumbnail} alt="" />
+        <div className="product-info">
+          <h2 className="product-title">{data.title}</h2>
+          <p className="product-price">{data.price}</p>
+          <p className="product-description">{data.description}</p>
+          <button className="buy-button">Buy Now</button>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default Product;
